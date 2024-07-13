48e68d9098ec3f0e79b033172e63865c00732945
Vallabhjoshi1998 <vallabh.g.joshi26.vj@gmail.com>
2024-07-13 20:36:37 +0530

    Added Constructor Injection instead of Autowired

--- src/main/java/com/vallabhjoshi/FirstWeekHomeworkApp/AliceAndHerBakery/CakeBaker.java
+++ src/main/java/com/vallabhjoshi/FirstWeekHomeworkApp/AliceAndHerBakery/CakeBaker.java
@@ -1,17 +1,20 @@
 package com.vallabhjoshi.FirstWeekHomeworkApp.AliceAndHerBakery;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
 public class CakeBaker {
 
-    @Autowired
-    public Frosting frosting;
 
-    @Autowired
-    public Syrup syrup;
+    //Constructor Dependency Injection - it is better to use this when are using it "only once" and if we want that no one should be able to change anything using
+    //the dependency injected here i.e. frosting and syrup
+    private final Frosting frosting;
+    private final Syrup syrup;
+
+    public CakeBaker(Frosting frosting, Syrup syrup){
+        this.frosting = frosting;
+        this.syrup = syrup;
+    }
 
     public void cakeBake(){
         frosting.getFrostingType();
